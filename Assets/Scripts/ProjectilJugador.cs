0b2c75bddd82b3c2e8e8aac657fa5eefec20d8ee
Mure <miguelmure@ceroca.cat>
2024-10-07 17:56:31 +0200

    GenerarNumeros Aleatorios

--- Assets/Scripts/ProjectilJugador.cs
+++ Assets/Scripts/ProjectilJugador.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ProjectilJugador : MonoBehaviour
 {
     private float _vel;
     private Vector2 maxPantalla;
